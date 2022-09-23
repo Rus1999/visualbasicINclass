5d60a60911664b18bb1287ca363473b4cdb5018c
Rus1999 <punyarit07@gmail.com>
2022-09-23 18:12:20 +0700

    radio button

---
+++ RadioButton/RadioButton/RadioButton/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SizeRadioGroup = New System.Windows.Forms.GroupBox()
+        Me.SizeL = New System.Windows.Forms.RadioButton()
+        Me.SizeM = New System.Windows.Forms.RadioButton()
+        Me.SizeS = New System.Windows.Forms.RadioButton()
+        Me.Title = New System.Windows.Forms.Label()
+        Me.ColorPink = New System.Windows.Forms.RadioButton()
+        Me.ColorBlack = New System.Windows.Forms.RadioButton()
+        Me.ColorBrown = New System.Windows.Forms.RadioButton()
+        Me.ColorRadioGroup = New System.Windows.Forms.GroupBox()
+        Me.Shrit1 = New System.Windows.Forms.Label()
+        Me.Shirt2 = New System.Windows.Forms.Label()
+        Me.SizeLabel = New System.Windows.Forms.Label()
+        Me.ConfirmBtn = New System.Windows.Forms.Button()
+        Me.SizeRadioGroup.SuspendLayout()
+        Me.ColorRadioGroup.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SizeRadioGroup
+        '
+        Me.SizeRadioGroup.Controls.Add(Me.SizeL)
+        Me.SizeRadioGroup.Controls.Add(Me.SizeM)
+        Me.SizeRadioGroup.Controls.Add(Me.SizeS)
+        Me.SizeRadioGroup.Location = New System.Drawing.Point(12, 41)
+        Me.SizeRadioGroup.Name = "SizeRadioGroup"
+        Me.SizeRadioGroup.Size = New System.Drawing.Size(97, 132)
+        Me.SizeRadioGroup.TabIndex = 0
+        Me.SizeRadioGroup.TabStop = False
+        Me.SizeRadioGroup.Text = "Size"
+        '
+        'SizeL
+        '
+        Me.SizeL.AutoSize = True
+        Me.SizeL.Location = New System.Drawing.Point(15, 86)
+        Me.SizeL.Name = "SizeL"
+        Me.SizeL.Size = New System.Drawing.Size(37, 24)
+        Me.SizeL.TabIndex = 4
+        Me.SizeL.TabStop = True
+        Me.SizeL.Text = "L"
+        Me.SizeL.UseVisualStyleBackColor = True
+        '
+        'SizeM
+        '
+        Me.SizeM.AutoSize = True
+        Me.SizeM.Location = New System.Drawing.Point(15, 56)
+        Me.SizeM.Name = "SizeM"
+        Me.SizeM.Size = New System.Drawing.Size(43, 24)
+        Me.SizeM.TabIndex = 3
+        Me.SizeM.TabStop = True
+        Me.SizeM.Text = "M"
+        Me.SizeM.UseVisualStyleBackColor = True
+        '
+        'SizeS
+        '
+        Me.SizeS.AutoSize = True
+        Me.SizeS.Location = New System.Drawing.Point(15, 26)
+        Me.SizeS.Name = "SizeS"
+        Me.SizeS.Size = New System.Drawing.Size(38, 24)
+        Me.SizeS.TabIndex = 2
+        Me.SizeS.TabStop = True
+        Me.SizeS.Text = "S"
+        Me.SizeS.UseVisualStyleBackColor = True
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.Location = New System.Drawing.Point(67, 9)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(120, 20)
+        Me.Title.TabIndex = 1
+        Me.Title.Text = "WOWZAA's Shirt"
+        '
+        'ColorPink
+        '
+        Me.ColorPink.AutoSize = True
+        Me.ColorPink.Location = New System.Drawing.Point(15, 26)
+        Me.ColorPink.Name = "ColorPink"
+        Me.ColorPink.Size = New System.Drawing.Size(57, 24)
+        Me.ColorPink.TabIndex = 5
+        Me.ColorPink.TabStop = True
+        Me.ColorPink.Text = "Pink"
+        Me.ColorPink.UseVisualStyleBackColor = True
+        '
+        'ColorBlack
+        '
+        Me.ColorBlack.AutoSize = True
+        Me.ColorBlack.Location = New System.Drawing.Point(15, 56)
+        Me.ColorBlack.Name = "ColorBlack"
+        Me.ColorBlack.Size = New System.Drawing.Size(65, 24)
+        Me.ColorBlack.TabIndex = 6
+        Me.ColorBlack.TabStop = True
+        Me.ColorBlack.Text = "Black"
+        Me.ColorBlack.UseVisualStyleBackColor = True
+        '
+        'ColorBrown
+        '
+        Me.ColorBrown.AutoSize = True
+        Me.ColorBrown.Location = New System.Drawing.Point(15, 86)
+        Me.ColorBrown.Name = "ColorBrown"
+        Me.ColorBrown.Size = New System.Drawing.Size(72, 24)
+        Me.ColorBrown.TabIndex = 7
+        Me.ColorBrown.TabStop = True
+        Me.ColorBrown.Text = "Brown"
+        Me.ColorBrown.UseVisualStyleBackColor = True
+        '
+        'ColorRadioGroup
+        '
+        Me.ColorRadioGroup.Controls.Add(Me.ColorPink)
+        Me.ColorRadioGroup.Controls.Add(Me.ColorBrown)
+        Me.ColorRadioGroup.Controls.Add(Me.ColorBlack)
+        Me.ColorRadioGroup.Location = New System.Drawing.Point(130, 41)
+        Me.ColorRadioGroup.Name = "ColorRadioGroup"
+        Me.ColorRadioGroup.Size = New System.Drawing.Size(105, 132)
+        Me.ColorRadioGroup.TabIndex = 5
+        Me.ColorRadioGroup.TabStop = False
+        Me.ColorRadioGroup.Text = "Color"
+        '
+        'Shrit1
+        '
+        Me.Shrit1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Shrit1.Location = New System.Drawing.Point(32, 241)
+        Me.Shrit1.Name = "Shrit1"
+        Me.Shrit1.Size = New System.Drawing.Size(186, 52)
+        Me.Shrit1.TabIndex = 6
+        '
+        'Shirt2
+        '
+        Me.Shirt2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Shirt2.Location = New System.Drawing.Point(82, 241)
+        Me.Shirt2.Name = "Shirt2"
+        Me.Shirt2.Size = New System.Drawing.Size(88, 128)
+        Me.Shirt2.TabIndex = 7
+        '
+        'SizeLabel
+        '
+        Me.SizeLabel.BackColor = System.Drawing.Color.White
+        Me.SizeLabel.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.SizeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.SizeLabel.Location = New System.Drawing.Point(99, 257)
+        Me.SizeLabel.Name = "SizeLabel"
+        Me.SizeLabel.Size = New System.Drawing.Size(53, 20)
+        Me.SizeLabel.TabIndex = 8
+        Me.SizeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ConfirmBtn
+        '
+        Me.ConfirmBtn.Location = New System.Drawing.Point(82, 196)
+        Me.ConfirmBtn.Name = "ConfirmBtn"
+        Me.ConfirmBtn.Size = New System.Drawing.Size(94, 29)
+        Me.ConfirmBtn.TabIndex = 9
+        Me.ConfirmBtn.Text = "Confirm"
+        Me.ConfirmBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(249, 410)
+        Me.Controls.Add(Me.ConfirmBtn)
+        Me.Controls.Add(Me.SizeLabel)
+        Me.Controls.Add(Me.Shirt2)
+        Me.Controls.Add(Me.Shrit1)
+        Me.Controls.Add(Me.ColorRadioGroup)
+        Me.Controls.Add(Me.Title)
+        Me.Controls.Add(Me.SizeRadioGroup)
+        Me.Name = "Form1"
+        Me.Text = "WOWZAA!!!"
+        Me.SizeRadioGroup.ResumeLayout(False)
+        Me.SizeRadioGroup.PerformLayout()
+        Me.ColorRadioGroup.ResumeLayout(False)
+        Me.ColorRadioGroup.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SizeRadioGroup As GroupBox
+    Friend WithEvents SizeL As Windows.Forms.RadioButton
+    Friend WithEvents SizeM As Windows.Forms.RadioButton
+    Friend WithEvents SizeS As Windows.Forms.RadioButton
+    Friend WithEvents Title As Label
+    Friend WithEvents ColorPink As Windows.Forms.RadioButton
+    Friend WithEvents ColorBlack As Windows.Forms.RadioButton
+    Friend WithEvents ColorBrown As Windows.Forms.RadioButton
+    Friend WithEvents ColorRadioGroup As GroupBox
+    Friend WithEvents Shrit1 As Label
+    Friend WithEvents Shirt2 As Label
+    Friend WithEvents SizeLabel As Label
+    Friend WithEvents ConfirmBtn As Button
+End Class
---
+++ RadioButton/RadioButton/RadioButton/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub ConfirmBtn_Click(sender As Object, e As EventArgs) Handles ConfirmBtn.Click
+        If SizeS.Checked Then
+            SizeLabel.Text = "S"
+        ElseIf SizeM.Checked Then
+            SizeLabel.Text = "M"
+        ElseIf SizeL.Checked Then
+            SizeLabel.Text = "L"
+        End If
+
+        If ColorPink.Checked Then
+            Shrit1.BackColor = Color.Pink
+            Shirt2.BackColor = Color.Pink
+            SizeLabel.BackColor = Color.Pink
+            SizeLabel.ForeColor = Color.Black
+        ElseIf ColorBlack.Checked Then
+            Shrit1.BackColor = Color.Black
+            Shirt2.BackColor = Color.Black
+            SizeLabel.BackColor = Color.Black
+            SizeLabel.ForeColor = Color.White
+        ElseIf ColorBrown.Checked Then
+            Shrit1.BackColor = Color.Brown
+            Shirt2.BackColor = Color.Brown
+            SizeLabel.BackColor = Color.Brown
+            SizeLabel.ForeColor = Color.White
+        End If
+    End Sub
+End Class
