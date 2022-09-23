577e6c82a61cb2b0f5fd8660bb3130a0b525d1ab
Rus1999 <punyarit07@gmail.com>
2022-09-23 18:13:37 +0700

    by Ref and by Value

---
+++ byRef_byValue/byRef_byValue/byRef_byValue/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ShowBtn = New System.Windows.Forms.Button()
+        Me.ValueLbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ShowBtn
+        '
+        Me.ShowBtn.Location = New System.Drawing.Point(12, 9)
+        Me.ShowBtn.Name = "ShowBtn"
+        Me.ShowBtn.Size = New System.Drawing.Size(198, 29)
+        Me.ShowBtn.TabIndex = 0
+        Me.ShowBtn.Text = "ShowBtn"
+        Me.ShowBtn.UseVisualStyleBackColor = True
+        '
+        'ValueLbl
+        '
+        Me.ValueLbl.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ValueLbl.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.ValueLbl.Location = New System.Drawing.Point(12, 41)
+        Me.ValueLbl.Name = "ValueLbl"
+        Me.ValueLbl.Size = New System.Drawing.Size(198, 225)
+        Me.ValueLbl.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(223, 275)
+        Me.Controls.Add(Me.ValueLbl)
+        Me.Controls.Add(Me.ShowBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ShowBtn As Button
+    Friend WithEvents ValueLbl As Label
+End Class
---
+++ byRef_byValue/byRef_byValue/byRef_byValue/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Function SqrootRef(ByRef x As Single) As Double
+        x ^= 0.5
+        SqrootRef = x
+    End Function
+
+    Private Function SqrootVal(ByVal x As Single) As Double
+        x ^= 0.5
+        SqrootVal = x
+    End Function
+
+    Private Sub ShowBtn_Click(sender As Object, e As EventArgs) Handles ShowBtn.Click
+        Dim r As Single = 9
+        Dim v As Single = 9
+
+        ValueLbl.Text = "by Ref " & vbNewLine & "r (before): " & r & vbNewLine & "sqroot: " & SqrootRef(r) & vbNewLine & "r (after): " & r & vbNewLine & vbNewLine & "by Val " & vbNewLine & "v (before): " & v & vbNewLine & "sqroot: " & SqrootVal(v) & vbNewLine & "v (after): " & v
+    End Sub
+End Class
